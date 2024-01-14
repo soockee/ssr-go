42cc89d9b6c4e5adb78ce649c2f286bb31723d6a
Soockee <gamingsockee@gmail.com>
2024-01-14 16:22:48 +0100

    fix: initial commit
    * add workflows
    + add templ for html generation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/soockee/ssr-go
+
+go 1.21.6
+
+require github.com/a-h/templ v0.2.513
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+
+	component := templates.hello("John")
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
+}
