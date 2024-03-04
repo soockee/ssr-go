5d2d147227cb75aa27ce9908da1e84e8c60f60d3
Soockee <gamingsockee@gmail.com>
2024-03-04 01:46:11 +0100

    fix: refactor https

--- api.go
+++ api.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"context"
 	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"net/http"
 	"os"
+	"runtime/debug"
 	"time"
 
 	"github.com/a-h/templ"
@@ -28,6 +28,7 @@ type ApiServer struct {
 	isProd        bool
 	domainName    string
 	ssl_cache_dir string
+	owner_email   string
 }
 
 func NewApiServer(listenAddr string, store Storage, fs http.Handler, isProd bool) *ApiServer {
@@ -39,7 +40,8 @@ func NewApiServer(listenAddr string, store Storage, fs http.Handler, isProd bool
 	}
 
 	server.domainName = os.Getenv("DOMAIN_NAME")
-	server.domainName = os.Getenv("SSL_CACHE_DIR")
+	server.ssl_cache_dir = os.Getenv("SSL_CACHE_DIR")
+	server.owner_email = os.Getenv("OWNER_EMAIL")
 	return server
 }
 
@@ -57,6 +59,27 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	})
 }
 
+func httpsRedirectMiddleware(logger zerolog.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					logger.Error().Any("err", err).Str("trace", string(debug.Stack())).Send()
+				}
+			}()
+
+			if r.TLS == nil {
+				http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+				return
+			}
+			next.ServeHTTP(w, r)
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
+
 func (s *ApiServer) Run() {
 	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}).
 		Level(zerolog.TraceLevel).
@@ -72,36 +95,56 @@ func (s *ApiServer) Run() {
 	loggingMiddleware := LoggingMiddleware(logger)
 	loggedRouter := loggingMiddleware(router)
 
+	var certManager *autocert.Manager
 	if s.isProd {
-		hostPolicy := func(ctx context.Context, host string) error {
-			if host == s.domainName {
-				return nil
-			}
-			logger.Error().Msgf("acme/autocert: only %s host is allowed", s.domainName)
-			return nil
-		}
-		m := &autocert.Manager{
+		certManager = &autocert.Manager{
 			Prompt:     autocert.AcceptTOS,
-			HostPolicy: hostPolicy,
+			HostPolicy: autocert.HostWhitelist(s.domainName),
 			Cache:      autocert.DirCache(s.ssl_cache_dir),
+			Email:      s.owner_email,
 		}
+
+		redirectMiddleware := httpsRedirectMiddleware(logger)
+		redirectRouter := redirectMiddleware(loggedRouter)
+
 		httpsServer := &http.Server{
 			ReadTimeout:  5 * time.Second,
 			WriteTimeout: 5 * time.Second,
 			IdleTimeout:  120 * time.Second,
-			Handler:      loggedRouter,
-			Addr:         ":443",
-			TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
+			Addr:         ":https",
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+				MinVersion:     tls.VersionTLS13, // improves cert reputation score at https://www.ssllabs.com/ssltest/
+			},
+			Handler: redirectRouter,
 		}
-		if err := httpsServer.ListenAndServeTLS("", ""); err != nil {
-			logger.Error().Err(err).Send()
-			os.Exit(1)
-		}
-	} else {
-		if err := http.ListenAndServe(s.listenAddr, loggedRouter); err != nil {
-			logger.Error().Err(err)
-			os.Exit(1)
-		}
+
+		go func() {
+			logger.Info().Msg("Starting HTTPS sever")
+			if err := httpsServer.ListenAndServeTLS("", ""); err != nil {
+				logger.Error().Err(err).Send()
+				os.Exit(1)
+			}
+		}()
+	}
+
+	httpServer := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Addr:         s.listenAddr,
+		Handler:      loggedRouter,
+	}
+
+	if certManager != nil {
+		// allow autocert handle Let's Encrypt auth callbacks over HTTP.
+		// it'll pass all other urls to our hanlder
+		httpServer.Handler = certManager.HTTPHandler(loggedRouter)
+	}
+
+	if err := httpServer.ListenAndServe(); err != nil {
+		logger.Error().Err(err)
+		os.Exit(1)
 	}
 }
 
